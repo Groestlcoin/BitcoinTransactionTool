cb098d18954264cd1317b56a81579630374e747e
Yokomoko <yokomoko_chapster@live.co.uk>
2019-07-22 12:31:47 +0100

    Slowly recreating MainWindow to be less terrible, and more scalable

---
+++ BitcoinTransactionTool/MainWindow - Copy.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace BitcoinTransactionTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow2 : Window
+    {
+        public MainWindow2()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
