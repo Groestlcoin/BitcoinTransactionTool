21798f58baf103ed8c9dc03c5e177a51303ed2e1
Yokomoko <yokomoko_chapster@live.co.uk>
2019-09-10 10:47:09 +0100

    Twatty fix

--- BitcoinTransactionTool/Services/TransactionServices/Chainz.cs
+++ BitcoinTransactionTool/Services/TransactionServices/Chainz.cs
@@ -9,7 +9,7 @@ namespace BitcoinTransactionTool.Services.TransactionServices {
     public class Chainz : TransactionApi {
         public override async Task<Response<List<UTXO>>> GetUTXO(List<SendingAddress> addrList) {
             Response<List<UTXO>> resp = new Response<List<UTXO>>();
-            string addresses = string.Join("|", addrList.Select(b => b.Address).ToArray());
+            //string addresses = string.Join("|", addrList.Select(b => b.Address).ToArray());
             string url = "https://chainz.cryptoid.info/grs/api.dws?q=unspent&key=632ba1ffd292&active=" + addresses;
             Response<JObject> apiResp = await SendApiRequestAsync(url);
             if (apiResp.Errors.Any()) {
@@ -21,12 +21,13 @@ namespace BitcoinTransactionTool.Services.TransactionServices {
             foreach (var item in apiResp.Result["unspent_outputs"]) {
                 UTXO u = new UTXO();
                 string script = item["script"].ToString();
-                u.TxHash = item["txid"].ToString();
-                u.OutIndex = (uint) item["vout"];
+                u.AddressHash160 = script.Substring("76a914".Length, script.Length - "76a91488ac".Length);
+                u.Address = item["addr"].ToString();
+                u.TxHash = item["tx_hash"].ToString();
 
                 u.Amount = (ulong) item["value"];
                 u.Confirmation = (int) item["confirmations"];
-                
+                u.OutIndex = (uint) item["tx_ouput_n"];
                 resp.Result.Add(u);
             }
 
